20cdc4a11a56516215a0599d42cd61e737a66aba
jonnythiem <j-thietke@t-online.de>
2017-09-25 12:30:58 +0200

    sone information added

--- System/src/interpreter/basic_interpreter.hh
+++ System/src/interpreter/basic_interpreter.hh
@@ -6,6 +6,13 @@
 class BasicInterpreter
 {
 	public:
+		struct header_Block {
+			uint64_t _LSN; 
+			uint32_t _Offset; //offset from the start of the partition
+			uint8_t _PID; //partition ID
+			uint16_t _padding1;
+			uint8_t _padding2;
+	};
 		BasicInterpreter();
 		~BasicInterpreter();
 
@@ -14,6 +21,8 @@ class BasicInterpreter
 
 	private:
 		byte* _pp;
+		static uint16_t _blocksize //4096 bytes
+		static uint16_t _headersize //16 bytes
 
 };
 
--- System/src/interpreter/fsip_interpreter.hh
+++ System/src/interpreter/fsip_interpreter.hh
@@ -5,17 +5,24 @@
 
 class FSIPInterpreter
 {
+	//0: block is free, 1: block is occupied
 	public:
+		struct header_FSIP {
+			uint32_t _freeBlocksCount;      // number of free Blocks in the managed part (numer of 0s)
+			uint32_t _nextFreeBlock;      // index of the next 0 (indicating a free Block)
+		};
 		FSIPInterpreter();
 		~FSIPInterpreter();
 
+
 	public:
 		void initNewFSIP(byte* aPP, uint64_t aLSN, uint32_t aOffset, uint8_t aPID, uint32_t aNoBlocks);
-		const int getBlock(byte* aPP);
+		const int getBlock(byte* aPP); //rtn: partitionoffset if free, -1 if FSIP full
 		void freeBlock(uint32_t aOffset);
 
 	private:
 		byte* _pp;
+		static uint16_t _headersize //8 bytes
 
 };
 
