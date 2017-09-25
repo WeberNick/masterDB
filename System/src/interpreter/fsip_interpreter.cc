91dd98decef91999c708fcb314449dea9f7faf4b
jonnythiem <j-thietke@t-online.de>
2017-09-25 20:36:59 +0200

    forgot to update nextFreeBlock

--- System/src/interpreter/fsip_interpreter.cc
+++ System/src/interpreter/fsip_interpreter.cc
@@ -24,12 +24,16 @@ const int FSIPInterpreter::getNewBlock(byte* aPP)
 void FSIPInterpreter::freeBlock(uint aOffset)
 {
 	aOffset-=_basicInterpreter.getPartitionOffset();
+
+	if(_header.nextFreeBlock > aOffset){
+		_header.nextFreeBlock=aOffset;
+	}
 	byte* lPP = _pp;
 	lPP+=(aOffset/8);
 	uint8_t lCurrByte = (uint8_t) *lPP;
 	uint8_t lBitindex = 7-(aOffset % 8);
 	uint8_t lMask = 1;
 	lMask << lBitindex;
-	lCurrByte = lCurrByte & lMask;
+	lCurrByte &= lMask;
 	//ändert das tatsächlich den Wert, oder ändert das nur was aufm Stack?
 }
