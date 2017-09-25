900eaef5cbf46b5b68f7fc731bbfe70ed9ec1c84
Aljoscha Narr <alnarr@mail.uni-mannheim.de>
2017-09-25 20:44:14 +0200

    getNewBlock() implemented in FSIPInterpreter

--- System/src/interpreter/fsip_interpreter.cc
+++ System/src/interpreter/fsip_interpreter.cc
@@ -17,8 +17,28 @@ void FSIPInterpreter::initNewFSIP(const byte* aPP, const uint64_t aLSN, const ui
 
 const int FSIPInterpreter::getNewBlock(byte* aPP)
 {
-	//todo
-	return 0;
+	BasicInterpreter lInterp = BasicInterpreter();
+	uint32_t lResult=-1;
+	uint32_t lReturnValue = _header._nextFreeBlock;
+	for(uint32_t i=lReturnValue;(lInterp->getBlockSize()-(_headersize+lInterp->getHeaderSize())/8-1;i++){
+		uint8_t lPartBits = (uint8_t) *aPP+(i*1);
+		if((~lPartBits)!=0){
+			for(uint8_t i= 7;i<=0;i--){
+				uint8_t	lTemp= lPartBits;
+				lTemp >> i;
+				if((temp % 2 == 0){
+					lResult=7-i;
+					lResult+=lInterp.getPartitionOffset()
+					uint8_t lMask=1;
+					lPartBits |= (lMask << lResult);
+					_nextFreeBlock=i;
+					break;
+				}
+			}
+			break; 
+		}
+	}
+	return lResult;
 }
 
 void FSIPInterpreter::freeBlock(uint aOffset)
