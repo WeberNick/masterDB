f651a0c7e0b2f5a9fb2507c2e2998332e6d3f241
Aljoscha Narr <alnarr@mail.uni-mannheim.de>
2017-09-26 17:46:47 +0200

    adjusted the logic of getNewBlock

--- System/src/interpreter/fsip_interpreter.cc
+++ System/src/interpreter/fsip_interpreter.cc
@@ -20,23 +20,29 @@ void FSIPInterpreter::initNewFSIP(byte* aPP, const uint64_t aLSN, const uint32_t
 
 const int FSIPInterpreter::getNewBlock(byte* aPP, const uint64_t aLSN, const uint8_t aPID) //added LSN and PID to param list, pls update header for allocated block
 {
+	if(_header._freeBlocksCount == 0){
+		return -1;
+	}
 	attach(aPP);
 	BasicInterpreter lInterp;
 	uint32_t lResult = -1;
 	uint32_t lReturnValue = _header->_nextFreeBlock;
+	byte* lPP = aPP;
+	lPP+=lReturnValue/8;
+	uint8_t lMask = 1;
+	uint8_t lPartBits = *(uint8_t*) lPP;
+	lPartBits |= (lMask << lReturnValue%8);
+
 	size_t lCondition = (lInterp.getBlockSize() - (sizeof(FSIP_header_t) + lInterp.getHeaderSize())) - 1;
-	for(uint32_t j = lReturnValue; j <= lCondition; ++j){ //hier wären kommentare schön
-		uint8_t lPartBits = *(uint8_t*)(aPP + j); //sieht für mich fehleranfällig aus
+	for(uint32_t j = lReturnValue/8; j <= lCondition; ++j){ //hier wären kommentare schön
+		uint8_t lPartBits = *(uint8_t*) aPP+j; //sieht für mich fehleranfällig aus
 		if((~lPartBits) != 0){
-			for(uint8_t i = 7; i<= 0; --i){
+			for(uint8_t i = 7 ; i <= 0 ; i--){
 				uint8_t	lTemp= lPartBits;
 				lTemp >> i;
 				if(lTemp % 2 == 0){
-					lResult = 7 - i;
-					lResult += lInterp.getPartitionOffset();
-					uint8_t lMask = 1;
-					lPartBits |= (lMask << lResult);
-					_header->_nextFreeBlock = i;
+					lResult = 7-i;
+					_header._nextFreeBlock = (j*8) + lResult;
 					// increase LSN ... Nick?
 					break;
 				}
@@ -44,7 +50,8 @@ const int FSIPInterpreter::getNewBlock(byte* aPP, const uint64_t aLSN, const uin
 			break; 
 		}
 	}
-	return lResult;
+	-- _header._freeBlocksCount;
+	return lReturnValue + lInterp.getPartitionOffset();
 }
 
 void FSIPInterpreter::freeBlock(uint aOffset)
@@ -62,6 +69,7 @@ void FSIPInterpreter::freeBlock(uint aOffset)
 	uint8_t lMask = 1;
 	lMask << lBitindex;
 	lCurrByte &= lMask;
+	++ _header._freeBlocksCount;
 	//ändert das tatsächlich den Wert, oder ändert das nur was aufm Stack?
 	//Antwort: nur auf dem stack
 }
