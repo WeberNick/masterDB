c7e8e01a2b620bd074a30155748357af4ec93945
WeberNick <weber.nick@icloud.com>
2017-10-02 17:33:53 +0200

    Refactored init method written by Jonas in fsip_interpreter.hh

--- System/src/interpreter/fsip_interpreter.cc
+++ System/src/interpreter/fsip_interpreter.cc
@@ -40,31 +40,31 @@ void FSIPInterpreter::initNewFSIP(byte* aPP, const uint64_t aLSN, const uint32_t
 
 const int FSIPInterpreter::getNewPage(byte* aPP, const uint64_t aLSN, const uint8_t aPID) //added LSN and PID to param list, pls update header for allocated block
 {
-	// if(_header->_freeBlocksCount == 0){
-	// 	return -1;
-	// }
-	// attach(aPP);
-	// BasicInterpreter lPageInterp;
-	// uint32_t lResult = -1;
-	// uint32_t lPosFreeBlock = _header->_nextFreeBlock;
-	// byte* lPP = aPP;
-	// lPP+=lPosFreeBlock/8;
-	// uint8_t lMask = 1;
-	// uint8_t lPartBits = *(uint8_t*) lPP;
-	// lPartBits |= (lMask << lPosFreeBlock%8);
+	if(_header->_freeBlocksCount == 0){
+		return -1;
+	}
+	attach(aPP);
+	BasicInterpreter lPageInterp;
+	uint32_t lResult = -1;
+	uint32_t lPosFreeBlock = _header->_nextFreeBlock;
+	byte* lPP = aPP;
+	lPP+=lPosFreeBlock/8;
+	uint8_t lMask = 1;
+	uint8_t lPartBits = *(uint8_t*) lPP;
+	lPartBits |= (lMask << lPosFreeBlock%8);
 
-	// size_t lCondition = ((_pageSize - sizeof(fsip_header_t))/8) - 1;
-	// for(uint32_t j = lPosFreeBlock/64; j <= lCondition; ++j){ //looping through FSIP with step 8 Bytes
-	// 	uint64_t lPartBytes = *(((uint64_t*) aPP)+j); //cast to 8 Byte Int Pointer, add the next j 8Byte block and dereference
-	// 	if((~lPartBytes) != 0){
-	// 		uint8_t lCalcFreePos = idx_lowest_bit_set<uint64_t>(~lPartBytes);
-	// 		_header->_nextFreeBlock = (j*64) + lResult;
-	// 		//change LSN
-	// 		break; 
-	// 	}
-	// }
-	// --(_header->_freeBlocksCount);
-	// return lPosFreeBlock + lPageInterp.getPageIndex();
+	size_t lCondition = ((_pageSize - sizeof(fsip_header_t))/8) - 1;
+	for(uint32_t j = lPosFreeBlock/64; j <= lCondition; ++j){ //looping through FSIP with step 8 Bytes
+		uint64_t lPartBytes = *(((uint64_t*) aPP)+j); //cast to 8 Byte Int Pointer, add the next j 8Byte block and dereference
+		if((~lPartBytes) != 0){
+			uint8_t lCalcFreePos = idx_lowest_bit_set<uint64_t>(~lPartBytes);
+			_header->_nextFreeBlock = (j*64) + lResult;
+			//change LSN
+			break; 
+		}
+	}
+	--(_header->_freeBlocksCount);
+	return lPosFreeBlock + lPageInterp.getPageIndex();
 }
 
 void FSIPInterpreter::freePage(uint aPageIndex)
