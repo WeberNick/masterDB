a0da839bc4b38e4441f71bcd05cad3f73d484041
Nicolas Wipfler <nicolas.wipfler@gmx.de>
2018-03-10 14:53:10 +0100

    Implement ThreadQueue and ThreadPool.

---
+++ System/src/threading/pool.hh
@@ -0,0 +1,20 @@
+/**
+ * @file    pool.hh
+ * @author  Nicolas Wipfler (nwipfler@mail.uni-mannheim.de)
+ * @brief   Class implementing a thread pool.
+ * @bugs    TBD
+ * @todos   TBD
+ */
+
+#ifndef POOL_HH
+#define POOL_HH
+
+class ThreadPool {
+  
+  public:
+
+  private:
+
+};
+
+#endif
